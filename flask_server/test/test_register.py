76ceea9a2528a84f3fde8884a313b6df7cae4382
dominic-source <chinonsodomnic@gmail.com>
2024-04-24 14:45:57 +0100

    integrate full registeration route in flask and update test cases

--- flask_server/test/test_register.py
+++ flask_server/test/test_register.py
@@ -3,9 +3,11 @@ from flask_testing import TestCase
 from flask import Flask
 from ..app import db
 from ..views.v1 import app_views
+import io
 
 class TestUser(TestCase):
     """This class is meant to test case that has to do with the user"""
+
     def create_app(self):
         self.app = Flask(__name__)
         self.app.config['TESTING'] = True
@@ -15,21 +17,54 @@ class TestUser(TestCase):
         with self.app.app_context():
             db.create_all()
         return self.app
+
     def tearDown(self):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
-    def test_signup(self):
+
+    def test_signup_v1(self):
         with self.client:
+            with open("/home/dominic-source/Pictures/Screenshots/dash.png", 'rb') as file:
+                buffer = io.BytesIO(file.read())
+            buffer.seek(0)
+            file = buffer, 'picture1.png'
             data2 = {
+            'first_name': 'Loveth',
+            'last_name': 'Ubah',
             'username': 'sdominic',
             'vehicle_number': 'sRaymond',
             'email': 'aymond@gmail.com',
             'password': '112345',
-            'confirm_password': '112345',
-            'phone_number': '233455632'
+            'phone_number': '233455632',
+            'status': 'busy',
+            'file': file
             }
-            response = self.client.post('/api/register', data=data2)
+            headers = {'Content-Type': 'multipart/form-data'}
+            response = self.client.post('/api/register', data=data2, headers=headers)
         data = response.json
         self.assertEqual(response.status_code, 200)
         self.assertTrue('username' in data)
+    
+    def test_signup_v2(self):
+        with self.client:
+            with open("/home/dominic-source/Pictures/Screenshots/dash.png", 'rb') as file:
+                buffer = io.BytesIO(file.read())
+            buffer.seek(0)
+            file = buffer, 'picture1.png'
+            data2 = {
+            'first_name': 'Loveth',
+            'last_name': 'Ubah',
+            'username': 'sdominic',
+            'vehicle_number': 'sRaymond',
+            'email': 'aymond@gmail.com',
+            'password': '112345',
+            'phone_number': '233455632',
+            'status': 'i no dey here',
+            'file': file
+            }
+            headers = {'Content-Type': 'multipart/form-data'}
+            response = self.client.post('/api/register', data=data2, headers=headers)
+        data = response.json
+        self.assertEqual(response.status_code, 401)
+        self.assertFalse('username' in data)
--- flask_server/views/v1/user.py
+++ flask_server/views/v1/user.py
@@ -2,7 +2,7 @@
 """
 from flask_server.views.v1 import app_views
 from flask import request, jsonify, make_response, session
-from flask_server.models import User
+from flask_server.models import User, Lga
 from flask_server import allowed_file, db, app, bcrypt
 from werkzeug.utils import secure_filename
 import os
@@ -10,6 +10,12 @@ import os
 @app_views.route('/register', strict_slashes=False, methods=['POST'])
 def register():
     """register a user"""
+    first_name = request.form.get('first_name')
+    if not first_name:
+        return jsonify({'data': 'first_name required!'}), 400
+    last_name = request.form.get('last_name')
+    if not last_name:
+        return jsonify({'data': 'last_name required!'}), 400
     username = request.form.get('username')
     if not username:
         return jsonify({'data': 'username required!'}), 400
@@ -20,11 +26,15 @@ def register():
     if not email:
         return jsonify({'data': 'email required!'}), 400
     phone_number = request.form.get('phone_number')
+    if not phone_number:
+        return jsonify({'data': 'phone_number required!'}), 400
+
     lga_id = request.form.get('lga_id')
     vehicle_number = request.form.get('vehicle_number')
     user_type = request.form.get('user_type')
     status = request.form.get('status')
     photo = None
+    # store images in static folder
     if 'file' in request.files:
         file = request.files['file']
         # If the user does not select a file, the browser submits an
@@ -41,16 +51,27 @@ def register():
     if user_check_number:
         return jsonify({'data': 'Phone number is taken!'})
     password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-    user = User(
-        username=username,
-        email=email,
-        vehicle_number=vehicle_number,
-        user_type=user_type,
-        lga_id=lga_id,
-        photo=photo,
-        phone_number=phone_number,
-        status=status,
-        password_hash=password_hash)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({'username': user.username, 'date': user.created_at, 'id': user.id}), 200
+    if lga_id:
+        lga = db.session.query(Lga).filter_by(id=lga_id).all()
+        if not lga:
+            lga_id = None
+    try:
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            vehicle_number=vehicle_number,
+            user_type=user_type,
+            lga_id=lga_id,
+            photo=photo,
+            phone_number=phone_number,
+            status=status,
+            password_hash=password_hash)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'username': user.username, 'date': user.created_at, 'id': user.id}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 401
+    # return jsonify({'username': user.username, 'date': user.created_at, 'id': user.id}), 200
