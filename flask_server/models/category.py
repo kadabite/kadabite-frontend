1917c7ef1a01221c656707292f69e8cca917b959
dominic-source <chinonsodomnic@gmail.com>
2024-05-10 16:19:43 +0100

    update models and run the flask migration file

--- flask_server/models/category.py
+++ flask_server/models/category.py
@@ -5,4 +5,4 @@ class Category(db.Model):
 	"""This is the users class"""
 	__tablename__ = 'category'
 	id = db.Column(db.Integer, primary_key=True)
-	category_name = db.Column(db.String(35), nullable=False)
+	name = db.Column(db.String(35), unique=True, nullable=False)
