083d3a4fd3244de4f3cee73622d4ac7ce43a349a
dominic-source <chinonsodomnic@gmail.com>
2024-05-15 23:54:57 +0100

    add mutation resolvers to resolver files

--- express_server/resolver/resolvers.js
+++ express_server/resolver/resolvers.js
@@ -1,15 +1,18 @@
 import { userMutationResolvers, userQueryResolvers } from './user&category.resolver';
 import { productQueryResolver, productMutationResolver } from './products.resolver';
+import { ordersMutationResolver, ordersQueryResolver } from './orders.resolver';
 
 const resolvers = {
   Query: {
     ...userQueryResolvers,
     ...productQueryResolver,
+    ...ordersQueryResolver,
   },
 
   Mutation: {
     ...userMutationResolvers,
     ...productMutationResolver,
+    ...ordersMutationResolver,
   },
 };
 module.exports = resolvers;
