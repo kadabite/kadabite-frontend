d24b015346fcd5405e01fb287b710a60da6efc1c
dominic-source <chinonsodomnic@gmail.com>
2024-05-20 20:04:37 +0100

    add the get order and get order item endpoint

--- flask_server/views/v1/__init__.py
+++ flask_server/views/v1/__init__.py
@@ -11,6 +11,16 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='consumer.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+def admin_route(func):
+    """A decorator that allows only admin to access a particular route"""
+    @wraps(func)
+    def admin(*args, **kwargs):
+        if not auth.is_admin():
+            return jsonify({'error': 'You need to be an admin to access this route!'}), 403
+        return func(*args, **kwargs)
+    return admin
+
+
 def protected_route(func):
     """Decorator that ensures a route is protected"""
     @wraps(func)
@@ -19,6 +29,22 @@ def protected_route(func):
             return jsonify({'error': 'An error occured, try to log in!'}), 403
         return func(*args, **kwargs)
     return protect
+
+
+def generate_orders(order):
+    if not order:
+        return []
+    return [{
+            'id': order_b.id,
+            'buyer_id': order_b.buyer_id,
+            'seller_id': order_b.seller_id,
+            'dispatcher_id': order_b.dispatcher_id,
+            'date_of _order': order_b.order_date_time,
+            'currency': order_b.currency,
+            'total_amount': order_b.total_amount,
+            'status': order_b.status,
+            'delivery_address': order_b.delivery_address,
+            } for order_b in order]
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api')
--- flask_server/views/v1/order.py
+++ flask_server/views/v1/order.py
@@ -1,11 +1,107 @@
 """ This module is used to manage the orders
 """
 import json
-from flask_server.views.v1 import app_views, protected_route, logger
+from flask_server.views.v1 import app_views, protected_route, logger, admin_route, generate_orders
 from flask import request, jsonify, make_response, session
 from flask_server.models import Product, Category, User, Order, OrderItem
 from flask_server import db
 from datetime import datetime
+from sqlalchemy.orm.exc import NoResultFound
+
+
+@app_views.route('/order_items/<id>', methods=['GET'], strict_slashes=False)
+@protected_route
+def get_all_items_of_order(id=None):
+    """This endpoint gets all the items associated with an order.
+
+    To access this endpoint, your user ID must be either a seller, buyer,
+    or dispatcher associated with the order.
+
+    Args:
+        id (int, mandatory): The ID of the order to retrieve items for.
+
+    Returns:
+        JSON: A JSON response containing the order items or an error message.
+
+    Raises:
+        HTTPException: 401 (Unauthorized) if the order ID is missing,
+                       or an error occurs during retrieval.
+    """
+
+    try:
+        if id is None:
+            return jsonify({'error': 'order id required'}), 401
+
+        user_id = session.get('user_id')
+
+        # Filter based on user role and provided ID
+        query = Order.query.filter(
+            (Order.seller_id == user_id) |
+            (Order.buyer_id == user_id) |
+            (Order.dispatcher_id == user_id)
+        ).filter_by(id=id)
+
+        orderitems = query.one().orderitems
+        items = [{ 
+            'id': order.id,
+            'product_id': order.product_id,
+            'quantity': order.quantity} for order in orderitems]
+        return jsonify(items)
+
+    except NoResultFound:
+        return jsonify({'error': 'Order not found'}), 404
+
+    except Exception as e:
+        db.session.rollback()
+        logger.error("Error occured:", exc_info=True)
+        return jsonify({'error': 'An error occured!'}), 401
+
+
+@app_views.route('/all_orders', methods=['GET'], strict_slashes=False)
+@admin_route
+@protected_route
+def get_all_order():
+    """This route is an admin route to get all orders"""
+    try:
+        orders = Order.query.all()
+        return jsonify(generate_orders(orders)) 
+    except Exception as e:
+        db.session.rollback()
+        logger.error("Error occured:", exc_info=True)
+        return jsonify({'error': 'An error occured!'}), 401
+
+
+@app_views.route('my_orders', methods=['GET'], strict_slashes=False)
+@protected_route
+def get_my_orders():
+    """This route returns all the users order as a buyer and/or seller and/or dispatcher
+        endpoint: my_orders
+
+        request arguments'/api/my_orders?seller=false&buyer=true&dispatcher=false':
+            seller: true | false
+            buyer: true | false
+            dispatcher: true | false
+    """
+    seller = request.args.get('seller', 'false')
+    buyer = request.args.get('buyer', 'false')
+    dispatcher = request.args.get('dispatcher', 'false')
+    data = {}
+    try:
+        id = session.get('user_id')
+        if seller.lower() == 'true':
+            order_b = Order.query.filter_by(buyer_id=id).all()
+            data['buyer'] =  generate_orders(order_b)
+        if buyer.lower() == 'true':
+            order_s = Order.query.filter_by(seller_id=id).all()
+            data['seller'] =  generate_orders(order_s)
+        if dispatcher.lower() == 'true':
+            order_d = Order.query.filter_by(dispatcher_id=id).all()
+            data['dispatcher'] = generate_orders(order_d)
+        return jsonify(data), 200
+    except Exception as e:
+        db.session.rollback()
+        logger.error("Error occured:", exc_info=True)
+        return jsonify({'error': 'An error occured!'}), 401
 
 
 @app_views.route('/order', methods=['POST'], strict_slashes=False)
@@ -51,7 +147,6 @@ def create_order():
         db.session.commit()
         return jsonify({'order_id': order_n.id}), 201
     except Exception as e:
-        print(e)
         db.session.rollback()
         logger.error("Error occured:", exc_info=True)
         return jsonify({'error': 'An error occured!'}), 401
