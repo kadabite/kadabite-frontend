9208f21a8410a988b1090a9c34ad8130c520a702
dominic-source <chinonsodomnic@gmail.com>
2024-05-23 21:46:01 +0100

    integrate an auto save middleware to carryout some action

--- express_server/models/order.js
+++ express_server/models/order.js
@@ -1,6 +1,9 @@
 import mongoose from  "mongoose";
 import { orderItemSchema } from "./orderItem";
 import { paymentSchema } from "./payment";
+import { Product } from "./product";
+import { OrderItem } from "./orderItem";
+
 
 const { Schema } = mongoose;
 
@@ -17,5 +20,19 @@ const orderSchema = new Schema({
   payment: [paymentSchema],
 });
 
+orderSchema.pre('save', async function(next) {
+  // this.totalAmount =
+  const items = this.orderItems;
+  let totalAmount = 0
+  for (const item of items) {
+    const orderitem = await OrderItem.findById(item._id.toString());
+    if (orderitem) {
+      const product = await Product.findById(orderitem.productId.toString());
+      totalAmount += product.price * orderitem.quantity;
+    }
+  }
+  this.totalAmount = totalAmount;
+  next();
+});
 const Order = mongoose.model('Order', orderSchema);
 module.exports = Order;
