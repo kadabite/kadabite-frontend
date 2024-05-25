30219305aca133131576cfb37a5c092161d2602c
dominic-source <chinonsodomnic@gmail.com>
2024-05-25 22:03:00 +0100

    add the create payment resolver

--- express_server/resolver/payment.resolver.js
+++ express_server/resolver/payment.resolver.js
@@ -1,6 +1,9 @@
 import { myLogger } from '../utils/mylogger';
 import { Payment } from '../models/payment';
 import Order from '../models/order';
+import { paymentStatus, paymentMethods, currency } from '../../configPayment.json'
+
+const availableCurrency = currency;
 
 export const paymentQueryResolver = {
   getMyPayment: async (_parent, { orderId }, { user }) => {
@@ -21,5 +24,33 @@ export const paymentQueryResolver = {
 }
 
 export const paymentMutationResolver = {
-
+  createPayment: async (_parent, {
+    orderId,
+    paymentMethod,
+    currency,
+    sellerAmount,
+    dispatcherAmount,
+      }, { user }) => {
+    try {
+      const order = await Order.findById(id=orderId)
+      if (!order) return {'message': 'Order not found'};
+      if (order.buyerId.toString() !== user.id) return {'message': 'Unauthorized'};
+      if (!paymentMethods.includes(paymentMethod)) return {'message': 'payment method is not allowed'};
+      if (!availableCurrency.includes(currency)) return {'message': 'currency not available for transaction'};
+      const pay = new Payment({
+        orderId,
+        paymentMethod,
+        currency,
+        sellerAmount,
+        dispatcherAmount
+      });
+      order.payment.push(pay._id);
+      await order.save()
+      await pay.save();
+      return {'message': 'Payment was successful!'};
+    } catch (error) {
+      myLogger.error('Error creating user: ' + error.message);
+      return null;
+    }
+  },
 }
