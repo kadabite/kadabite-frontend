fb3abbf895f16b8324bd720a300a499f54e54309
dominic-source <chinonsodomnic@gmail.com>
2024-05-27 12:23:07 +0100

    update payment model and migrate changes to model

--- flask_server/models/payment.py
+++ flask_server/models/payment.py
@@ -9,7 +9,7 @@ def validate_payment_method():
 
 def validate_status():
 	"""Ensure that payment status is correct"""
-	return "payment_status IN ('inprocess', 'unpaid', 'paid')"
+	return "seller_payment_status IN ('inprocess', 'unpaid', 'paid')"
 
 def validate_currency():
 	"""Ensure that currency is accepted"""
@@ -20,7 +20,7 @@ class Payment(db.Model):
 	"""This is the users class"""
 	__tablename__ = 'payment'
 	id = db.Column(db.Integer, primary_key=True)
-	payment_method = db.Column(db.String(10), CheckConstraint(text(validate_payment_method())), defaut='transfer')
+	payment_method = db.Column(db.String(10), CheckConstraint(text(validate_payment_method())), default='transfer')
 	seller_payment_status = db.Column(db.String(10), CheckConstraint(text(validate_status())), default='unpaid')
 	dispatcher_payment_status = db.Column(db.String(10), CheckConstraint(text(validate_status())), default='unpaid')	
 	payment_date_time = db.Column(db.DateTime, nullable=True)
