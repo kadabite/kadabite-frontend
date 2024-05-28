7daa9c125c2d617afdf1edc4c68d67ea0ca585f1
dominic-source <chinonsodomnic@gmail.com>
2024-05-28 15:58:17 +0100

    make utils deletemodels eslint compliant

--- express_server/utils/managedata/deletemodels.js
+++ express_server/utils/managedata/deletemodels.js
@@ -1,8 +1,5 @@
 import { myLogger } from '../mylogger';
-import Order from '../../models/order';
 import { OrderItem } from '../../models/orderItem';
-import { User } from '../../models/user';
-import { Product } from '../../models/product'
 
 export const deleteOrderItems = async (createdItem) => {
   for (const id of createdItem) {
