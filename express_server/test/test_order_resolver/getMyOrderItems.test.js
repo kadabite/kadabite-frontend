5264fd9f950098499a124851ba080b52d40cbce5
dominic-source <chinonsodomnic@gmail.com>
2024-06-02 21:50:54 +0100

    create: create additional test cases for get order items

--- express_server/test/test_order_resolver/getMyOrderItems.test.js
+++ express_server/test/test_order_resolver/getMyOrderItems.test.js
@@ -48,4 +48,69 @@ describe('getMyOrderitems', function() {
     expect(stubFind.calledOnce).to.be.true;
     expect(result).to.be.deep.equal(items);
   });
+
+  it('should return an empty array if order is not found', async function() {
+    const stubFind = stub(Order, 'find').resolves([]);
+
+    const result = await ordersQueryResolver.getMyOrderItems(
+        null,
+        { orderId: new Types.ObjectId()},
+        {
+            user: { id: new Types.ObjectId() }
+        }
+      );
+    
+    expect(stubFind.calledOnce).to.be.true;
+    expect(result).to.be.deep.equal([]);
+  });
+
+  it('should return an empty array if an exception is thrown', async function(){
+    const stubFind = stub(Order, 'find').throws(new Error('An exception occurred!'))
+    const result = await ordersQueryResolver.getMyOrderItems(
+      null,
+      { orderId: new Types.ObjectId()},
+      {
+          user: { id: new Types.ObjectId() }
+      }
+    );
+  
+    expect(stubFind.calledOnce).to.be.true;
+    expect(result).to.be.deep.equal([]);
+  });
+
+  it('should return an empty array if an exception is thrown in orderitem', async function(){
+    const orders = [
+      {
+        id: '1',
+        buyerId: '1',
+        sellerId: '2',
+        dispatcherId: '3',
+        orderItems: ['1', '2'],
+        status: 'pending',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ];
+    const items = [
+        {
+          productId: '1',
+          quantity: 100
+        }
+    ];
+    const stubFind = stub(Order, 'find').resolves(orders);
+
+    const stubFindItems = stub(OrderItem, 'find').throws(new Error());
+
+    const result = await ordersQueryResolver.getMyOrderItems(
+        null,
+        { orderId: new Types.ObjectId()},
+        {
+            user: { id: new Types.ObjectId() }
+        }
+      );
+    
+    expect(stubFindItems.calledOnce).to.be.true;
+    expect(stubFind.calledOnce).to.be.true;
+    expect(result).to.be.deep.equal([]);
+  });
 });
