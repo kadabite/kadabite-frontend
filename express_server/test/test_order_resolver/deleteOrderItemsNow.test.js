c85cdfcde806cd98f201d835ac3606cd14825f6e
dominic-source <chinonsodomnic@gmail.com>
2024-06-03 14:27:21 +0100

    create: add test cases for update Order items now endpoint

---
+++ express_server/test/test_order_resolver/deleteOrderItemsNow.test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import { Types } from 'mongoose';
+import Order from '../../models/order';
+import { ordersMutationResolver } from '../../resolver/orders.resolver';
+import { myLogger } from '../../utils/mylogger';
+
+const expect = chai.expect;
+const { stub, restore } = sinon;
+
+describe('deleteOrderItemsNow', function() {
+  afterEach(function() {
+    restore();
+  });
+
+  it('should return message after successfully deleting order items', async function() {
+    const ids = [new Types.ObjectId().toString(), new Types.ObjectId().toString()];
+    const result = await ordersMutationResolver.deleteOrderItemsNow(null, { ids });
+    expect(result).to.have.property('message', 'Order items may have been deleted successfully!');
+  });
+});
