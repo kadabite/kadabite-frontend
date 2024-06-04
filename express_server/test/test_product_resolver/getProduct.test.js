c82c0d4795d7a5d12a7f078f90629aac2d551ada
dominic-source <chinonsodomnic@gmail.com>
2024-06-04 19:00:48 +0100

    write: test cases for the get product endpoint

---
+++ express_server/test/test_product_resolver/getProduct.test.js
@@ -0,0 +1,77 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import { Product } from '../../models/product';
+import { productQueryResolver } from '../../resolver/products.resolver';
+import { Types } from 'mongoose';
+
+const expect = chai.expect;
+const { stub, restore } = sinon;
+
+describe('getProduct', function() {
+  afterEach(function() {
+    restore();
+  });
+
+  it('should return a product', async function() {
+    const product = [{
+      _id: new Types.ObjectId(),
+      name: 'Product 1',
+      description: 'Product 1 description',
+      price: 100,
+      currency: 'Dollar',
+      categoryId: new Types.ObjectId()
+    }];
+    const findByIdStub = stub(Product, 'findById').resolves(product);
+
+    const result = await productQueryResolver.getProduct(null, { id: product._id });
+    expect(result).to.be.deep.equal(product);
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+  it('should return null if product is not found', async function() {
+    const product = [{
+      _id: new Types.ObjectId(),
+      name: 'Product 1',
+      description: 'Product 1 description',
+      price: 100,
+      currency: 'Dollar',
+      categoryId: new Types.ObjectId()
+    }];
+    const findByIdStub = stub(Product, 'findById').resolves(null);
+
+    const result = await productQueryResolver.getProduct(null, { id: product._id });
+    expect(result).to.be.null;
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+
+  it('should return null if an error occurs', async function() {
+    const product = [{
+      _id: new Types.ObjectId(),
+      name: 'Product 1',
+      description: 'Product 1 description',
+      price: 100,
+      currency: 'Dollar',
+      categoryId: new Types.ObjectId()
+    }];
+    const findByIdStub = stub(Product, 'findById').throws(new Error('Error'));
+
+    const result = await productQueryResolver.getProduct(null, { id: product._id });
+    expect(result).to.be.null;
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+
+  it('should return null if an exception occurs', async function() {
+    const product = [{
+      _id: new Types.ObjectId(),
+      name: 'Product 1',
+      description: 'Product 1 description',
+      price: 100,
+      currency: 'Dollar',
+      categoryId: new Types.ObjectId()
+    }];
+    const findByIdStub = stub(Product, 'findById').throws(new Error('Error'));
+
+    const result = await productQueryResolver.getProduct(null, { id: product._id });
+    expect(result).to.be.null;
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+});
