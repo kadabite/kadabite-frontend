614591dc14ee8db4a8fef08a512fb7f40b048e1b
dominic-source <chinonsodomnic@gmail.com>
2024-06-04 18:59:55 +0100

    write: test cases for the get all products endpoint

---
+++ express_server/test/test_product_resolver/getAllProductsOfUsersByCategor.test.js
@@ -0,0 +1,62 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import { Product } from '../../models/product';
+import { User } from '../../models/user';
+import { productQueryResolver } from '../../resolver/products.resolver';
+import { Types } from 'mongoose';
+
+const expect = chai.expect;
+const { stub, restore } = sinon;
+
+describe('getAllProductsOfUsersByCategory', function() {
+  afterEach(function() {
+    restore();
+  });
+
+  it('should return all products of a user by category', async function() {
+    const user = {
+      id: new Types.ObjectId(),
+      products: [{
+        _id: new Types.ObjectId(),
+        name: 'Product 1',
+        description: 'Product 1 description',
+        price: 100,
+        currency: 'Dollar',
+        categoryId: new Types.ObjectId()
+      }]
+    };
+    const findByIdStub = stub(User, 'findById').returns({ populate: stub().resolves(user) });
+    const result = await productQueryResolver.getAllProductsOfUsersByCategory(null, { categoryId: user.products[0].categoryId.toString() }, { user });
+    expect(result).to.be.deep.equal(user.products);
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+
+  it('should return null if an error occurs', async function() {
+    const user = {
+      id: new Types.ObjectId(),
+      products: [{
+        _id: new Types.ObjectId(),
+        name: 'Product 1',
+        description: 'Product 1 description',
+        price: 100,
+        currency: 'Dollar',
+        categoryId: new Types.ObjectId()
+      }]
+    };
+    const findByIdStub = stub(User, 'findById').throws(new Error('Error occurred'));
+    const result = await productQueryResolver.getAllProductsOfUsersByCategory(null, { categoryId: user.products[0].categoryId.toString() }, { user });
+    expect(result).to.be.null;
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+
+  it('should return null if user does not have products', async function() {
+    const user = {
+      id: new Types.ObjectId(),
+      products: []
+    };
+    const findByIdStub = stub(User, 'findById').returns({ populate: stub().resolves(user) });
+    const result = await productQueryResolver.getAllProductsOfUsersByCategory(null, { categoryId: new Types.ObjectId().toString() }, { user });
+    expect(result).to.be.empty;
+    expect(findByIdStub.calledOnce).to.be.true;
+  });
+});
