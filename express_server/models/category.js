2b7ddfa60e3b37b619964ca2a0478c894918027c
dominic-source <chinonsodomnic@gmail.com>
2024-06-06 20:23:32 +0100

    update categories to limit name length

--- express_server/models/category.js
+++ express_server/models/category.js
@@ -3,7 +3,7 @@ import mongoose from  "mongoose";
 const { Schema } = mongoose;
 
 const categorySchema = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true , maxlength: 100},
 });
 
 const Category = mongoose.model('Category', categorySchema);
