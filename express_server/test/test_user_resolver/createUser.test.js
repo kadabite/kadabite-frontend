76a2002371f75d82b3386c8b073321934324027f
dominic-source <chinonsodomnic@gmail.com>
2024-06-06 21:45:13 +0100

    write: create test cases for create user

---
+++ express_server/test/test_user_resolver/createUser.test.js
@@ -0,0 +1,57 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import { User } from '../../models/user';
+import { userMutationResolvers } from '../../resolver/user&category.resolver';
+import { Types } from 'mongoose';
+
+const expect = chai.expect;
+const { stub, restore } = sinon;
+
+describe('createUser', function() {
+  afterEach(function() {
+    restore();
+  });
+
+  it('should create a user', async function() {
+    const user = { 
+        username: 'username',
+        email: 'email',
+        passwordHash: 'passwordHash',
+        phoneNumber: 'phoneNumber',
+        userType: 'userType',
+        status: 'status',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        lgaId: 'lgaId', 
+        vehicleNumber: '123456',
+        }
+
+    const userCreateStub = stub(User.prototype, 'save').resolves(user);
+    const result = await userMutationResolvers.createUser(null, { user });
+    expect(result.username).to.deep.equal(user.username);
+    expect(userCreateStub.calledOnce).to.be.true;
+  });
+
+  it('should throw an error if an exception occurs', async function() {
+    const user = { 
+        username: 'username',
+        email: 'email',
+        passwordHash: 'passwordHash',
+        phoneNumber: 'phoneNumber',
+        userType: 'userType',
+        status: 'status',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        lgaId: 'lgaId', 
+        vehicleNumber: '123456',
+        }
+
+    const userCreateStub = stub(User.prototype, 'save').throws(new Error('Error creating user'));
+    try {
+      await userMutationResolvers.createUser(null, { user });
+    } catch (error) {
+      expect(error.message).to.equal('Error creating user');
+      expect(userCreateStub.calledOnce).to.be.true;
+    }
+  });
+});
