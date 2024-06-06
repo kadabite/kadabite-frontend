b3f7219a8699df8e34686a1a20abbc6273234eb9
dominic-source <chinonsodomnic@gmail.com>
2024-06-06 21:45:49 +0100

    write: create test cases for deleting category

---
+++ express_server/test/test_user_resolver/deleteCategory.test.js
@@ -0,0 +1,50 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import Category from '../../models/category';
+import { userMutationResolvers } from '../../resolver/user&category.resolver';
+import { Types } from 'mongoose';
+
+const expect = chai.expect;
+const { stub, restore } = sinon;
+
+describe('deleteCategory', function() {
+  afterEach(function() {
+    restore();
+  });
+
+  it('should delete a category', async function() {
+    const category = {
+      _id: new Types.ObjectId(),
+      name: 'Consumable Products|Food|Vegetables',
+    };
+
+    const categoryStub = stub(Category, 'findByIdAndDelete').resolves(category);
+
+    const result = await userMutationResolvers.deleteCategory(null, {
+      id: category._id,
+    });
+
+    expect(result).to.deep.equal({'message': 'Category has been deleted successfully!'});
+    expect(categoryStub.calledOnce).to.be.true;
+    expect(categoryStub.calledWith(category._id)).to.be.true;
+  });
+
+  it('should throw an error if the category is not found', async function() {
+    const category = {
+      _id: new Types.ObjectId(),
+      name: 'Consumable Products|Food|Vegetables',
+    };
+
+    const categoryStub = stub(Category, 'findByIdAndDelete').resolves(null);
+
+    try {
+      await userMutationResolvers.deleteCategory(null, {
+        id: category._id,
+      });
+    } catch (error) {
+      expect(error.message).to.equal('Category not found');
+    }
+    expect(categoryStub.calledOnce).to.be.true;
+    expect(categoryStub.calledWith(category._id)).to.be.true;
+  });
+});
