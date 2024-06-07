7421cec359a7e62f002600ba8fc60fac1140c064
dominic-source <chinonsodomnic@gmail.com>
2024-06-07 14:46:40 +0100

    upgrade logger function

--- express_server/utils/mylogger.js
+++ express_server/utils/mylogger.js
@@ -1,10 +1,28 @@
 import winston from 'winston';
 
-export const myLogger = (() =>  winston.createLogger({
+export const myLogger = (() =>  { 
+  
+  if (process.env.NODE_ENV === 'test') {
+    return winston.createLogger({
+      transports: [
+        new winston.transports.Console({
+          silent: true,
+        }),
+      ],
+    });
+  }
+  
+  return winston.createLogger({
   level: 'info',
-  format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
   transports: [
     new winston.transports.File({ filename: 'logs/request.log', level: 'info'}),
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
   ],
-}))();
+})
+})();
