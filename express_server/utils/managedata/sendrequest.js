afb5a1bdb64979e0ac53c2279707ad987db1bd06
dominic-source <chinonsodomnic@gmail.com>
2024-06-13 22:21:44 +0100

    creat utility function for sending request

---
+++ express_server/utils/managedata/sendrequest.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch';
+// check if the file extension for a file upload is allowed
+export async function authRequest(reqHeader) {
+    // Login logic using the RESTful API (already implemented)
+  return await fetch(`${process.env.DELIVER_URL}/api/authenticate`, {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json',
+      'authorization': reqHeader
+    },
+  });
+};
+
+export async function loginMe(email, password) {
+  return await fetch(`${process.env.DELIVER_URL}/api/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
+};
+  
