33907f3e65d0fe37a66ecbfba74b04a2b4cfc870
dominic-source <chinonsodomnic@gmail.com>
2024-06-13 22:20:07 +0100

    create the authentication route controllers

--- express_server/controllers/userControllers.js
+++ express_server/controllers/userControllers.js
@@ -4,6 +4,33 @@ import jwt from 'jsonwebtoken';
 import { myLogger } from '../utils/mylogger';
 
 class UserControllers {
+  static async authenticate(req, res) {
+     // Check the authorization header of the user and return error if None or error
+     const reqHeader = req.headers.authorization;
+     if (!reqHeader || !reqHeader.startsWith('Bearer ')) {
+       return res.status(401).json({'message': 'User is not authenticated'});
+     };
+      // Verify the token if it is authentic, return error if not authentic
+      const token = reqHeader.split(' ')[1] || '';
+      const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+
+      if (!decoded) {
+        // This format must be maintained!
+        return res.status(401).json({'message': 'User is not authenticated'});
+      };
+      const user = await User.findById(decoded.userId);
+      if (!user || !user.isLoggedIn || user.isDeleted) {
+        // This format must be maintained!
+        return res.status(401).json({'message': 'User is not authenticated'});
+      };
+
+      if (user.username === 'admin' || user.email === 'admin@deliver.com') {
+        return res.status(200).json({ user, isAdmin: true });
+      } else {
+        return res.status(200).json({ user, isAdmin: false });
+      }
+  }
+
   static async login(req, res) {
     // this login route is used to login user from the graphql endpoint
 
@@ -12,7 +39,7 @@ class UserControllers {
     const password = req.body.password || '';
     // Verify if the email and password are authentic
     if (email == '' || password == '') {
-      return res.status(401).json({'error': 'provide more information'});
+      return res.status(401).json({'message': 'provide more information'});
     }
     try {
       // find the user based on the email and return error if the user does not exist
@@ -40,13 +67,13 @@ class UserControllers {
     const id = req.body.id;
     if (!id || typeof id != 'string') {
       // delete file when this error occurs
-      return res.status(401).json({'error': 'Id is required!'});
+      return res.status(401).json({'message': 'Id is required!'});
     }
     try {
       const user = await User.findByIdAndUpdate(id, { photo: req.file.filename});
       if (!user) {
         // delete file when this error occurs
-        return res.status(404).json({'error': 'not found'});
+        return res.status(404).json({'message': 'not found'});
       }
     } catch(error) {
       myLogger.error('Error creating user: ' + error.message);
