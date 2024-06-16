0bdfba31eaffb7d2d1472c775ad4cff887ce4a05
dominic-source <chinonsodomnic@gmail.com>
2024-06-16 19:39:25 +0100

    update request utility

--- express_server/utils/managedata/sendrequest.js
+++ express_server/utils/managedata/sendrequest.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 // check if the file extension for a file upload is allowed
 export async function authRequest(reqHeader) {
   // Login logic using the RESTful API (already implemented)
-  return await fetch(`${process.env.DELIVER_URL}/api/authenticate`, {
+  return await fetch(`${process.env.DELIVER_URL}/api/authenticateAndAuthorize`, {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
