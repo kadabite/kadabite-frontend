f98b8cfd45c8194d340ede10919f2e517749d6a1
dominic-source <chinonsodomnic@gmail.com>
2024-06-16 19:35:55 +0100

    integrate rate limit to all express endpoint and graphql resolvers

--- express_server/app.js
+++ express_server/app.js
@@ -10,6 +10,11 @@ import resolvers from './resolver/resolvers';
 import router from './routes';
 import { logMiddleware } from './middlewares/logMiddleware';
 import process from 'process';
+import { rateLimit } from 'express-rate-limit'
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { applyMiddleware } from 'graphql-middleware';
+import { shield } from 'graphql-shield';
+import { createRateLimitRule } from 'graphql-rate-limit';
 
 // initialize express server
 const app = express();
@@ -35,20 +40,60 @@ mongoose.connect(uri)
 const corsOptions = {
   origin: 'https://studio.apollographql.com',
   optionsSuccessStatus: 200,
-  methods: ['GET', 'PUT', 'POST'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
 }
 
-app.use('/api', logMiddleware);
-app.use(cors(corsOptions));
+// create rate limit rule
+const rateLimitRule = createRateLimitRule({
+  identifyContext: (context) => context.req.headers['x-forwarded-for'] || context.req.connection.remoteAddress || context.req.ip,
+  max: 100,
+  window: '15m'
+});
+
+// create permissions for some resolvers
+const permissions = shield({
+  Mutation: {
+    createUser: rateLimitRule,
+    updateUser: rateLimitRule,
+    deleteUser: rateLimitRule,
+  },
+});
+
+// Create executable schema with permissions middleware
+const schema = applyMiddleware(
+  makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  }),
+  permissions
+);
 
 // Parse incoming request bodies
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Logs all incoming requests
+app.use(logMiddleware);
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Create an instance of express-rate-limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 200,
+  message: "Too many requests from this IP, please try again after 15 minutes"
+})
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+
 app.use('/api', router)
 
 // Start Apollo server
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
+  csrfPrevention: true,
+  cache: 'bounded',
 });
 
 
