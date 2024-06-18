1fa675ec8abbc8fb09af4db0f89a23b36a47d94c
dominic-source <chinonsodomnic@gmail.com>
2024-06-18 23:49:31 +0100

    updated testcase for: : getMyOrders.test.js

--- express_server/test/test_order_resolver/getMyOrders.test.js
+++ express_server/test/test_order_resolver/getMyOrders.test.js
@@ -14,37 +14,50 @@ describe('getMyOrders', function() {
   });
 
   it('should get all orders where buyerId is equal to userId', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+         }
+      })
+    });
+
     const orders = [
-        {
-          id: '1',
-          buyerId: '1',
-          sellerId: '2',
-          dispatcherId: '3',
-          orderItems: ['1', '2'],
-          status: 'pending',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          id: '2',
-          buyerId: '1',
-          sellerId: '2',
-          dispatcherId: '3',
-          orderItems: ['1', '2'],
-          status: 'pending',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ];
+      {
+        id: '1',
+        buyerId: '1',
+        sellerId: '2',
+        dispatcherId: '3',
+        orderItems: ['1', '2'],
+        status: 'pending',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '2',
+        buyerId: '1',
+        sellerId: '2',
+        dispatcherId: '3',
+        orderItems: ['1', '2'],
+        status: 'pending',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
       const findStub = stub(Order, 'find').resolves(orders);
       const result = await ordersQueryResolver.getMyOrders(
         null,
         null,
-        {
-            user: { id: new Types.ObjectId() }
-        }
-      )
-      expect(result).to.be.deep.equal(orders);
+        { req: {
+          headers: {
+           authorization: "fakeString"
+          } 
+         }
+      });
+      expect(result.statusCode).to.equal(200);
+      expect(result.ok).to.be.true;
       expect(findStub.calledOnce).to.be.true;
   });
 
@@ -52,17 +65,53 @@ describe('getMyOrders', function() {
     // const orderId = new Types.ObjectId();
     const userId = new Types.ObjectId();
 
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: userId,
+         }
+      })
+    });
+
     stub(Order, 'find').throws(new Error('An error occurred'));
 
-    const result = await ordersQueryResolver.getMyOrders(null, null, { user: { id: userId.toString() } });
-    expect(result).to.deep.equal([]);
+    const result = await ordersQueryResolver.getMyOrders(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       } 
+    });
+    expect(result.statusCode).to.equal(500);
+    expect(result.message).to.equal('An error occurred!');
   });
 
   it('should throw an empty array if no order was found', async function(){
     const userId = new Types.ObjectId();
-    stub(Order, 'find').resolves([])
-    const result = await ordersQueryResolver.getMyOrders(null, null, { user: { id: userId.toString() } });
-    expect(result).to.deep.equal([]);
-  });
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: userId,
+         }
+      })
+    });
 
+    stub(Order, 'find').resolves([])
+    const result = await ordersQueryResolver.getMyOrders(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      });
+    expect(result).to.deep.equal({ ordersData: [], statusCode: 200, ok: true });
+  });
 });
