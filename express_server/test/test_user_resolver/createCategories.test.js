839c2cefeef388c7c31d8320c415a3185e9ddc0d
dominic-source <chinonsodomnic@gmail.com>
2024-06-19 00:03:05 +0100

    updated testcase for  createCategories

--- express_server/test/test_user_resolver/createCategories.test.js
+++ express_server/test/test_user_resolver/createCategories.test.js
@@ -14,6 +14,18 @@ describe('createCategories', function() {
   });
 
   it('should create categories from an array of categories and return sucessfully', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+         },
+        isAdmin: true
+      }),
+      status: 200
+    });
+
     const categories = [
       {
         _id: new Types.ObjectId(),
@@ -28,19 +40,36 @@ describe('createCategories', function() {
     const categoryStub = stub(Category, 'findOne').resolves(null);
     const categoryStub2 = stub(Category.prototype, 'save').resolves(categories);
 
-    const result = await userMutationResolvers.createCategories(null, {
+    const result = await userMutationResolvers.createCategories(
+      null, {
       name: [
         'Consumable Products|Food|Vegetables',
         'Consumable Products|Food|Fruits',
-      ],
+      ]},
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
     });
 
-    expect(result).to.deep.equal({'message': 'Many categories have been created successfully!'});
+    expect(result).to.deep.equal({ 'message': 'Many categories have been created successfully!', ok: true, statusCode: 201 });
     expect(categoryStub.calledTwice).to.be.true;
     expect(categoryStub2.calledTwice).to.be.true;
   });
   
   it('should create categories from an array of categories and return an error', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+        },
+        isAdmin: true
+      }),
+      status: 200
+    });
     const categories = [
       {
         _id: new Types.ObjectId(),
@@ -54,72 +83,145 @@ describe('createCategories', function() {
 
     const categoryStub = stub(Category, 'findOne').resolves({});
 
-    try {
-      await userMutationResolvers.createCategories(null, {
-        name: [
-          'Consumable Products|Food|Vegetables',
-          'Consumable Products|Food|Fruits',
-        ],
-      });
-    } catch (error) {
-      expect(error.message).to.equal('Category already exists');
-      expect(categoryStub.calledOnce).to.be.true;
-    }
+    const result = await userMutationResolvers.createCategories(null, {
+      name: [
+        'Consumable Products|Food|Vegetables',
+        'Consumable Products|Food|Fruits',
+      ],
+    },
+    { req: {
+      headers: {
+        authorization: "fakeString"
+      } 
+      }
+    });
+    expect(result).to.deep.equal({ message: 'Category already exists', statusCode: 400, ok: false});
+    expect(categoryStub.calledOnce).to.be.true;
   });
 
   it('should return an error if the name is not an array', async function() {
-    try {
-      await userMutationResolvers.createCategories(null, {
-        name: 'Consumable Products|Food|Vegetables',
-      });
-    } catch (error) {
-      expect(error.message).to.equal('Name must be an array');
-    }
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+        },
+        isAdmin: true
+      }),
+      status: 200
+    });
+    const result = await userMutationResolvers.createCategories(null, {
+      name: 'Consumable Products|Food|Vegetables',
+    },
+    { req: {
+      headers: {
+        authorization: "fakeString"
+      } 
+      }
+    });
+    expect(result).to.deep.equal({ message: 'Name must be an array', statusCode: 400, ok: false });
   });
 
   it('should return an error if the name is not a string', async function() {
-    try {
-      await userMutationResolvers.createCategories(null, {
-        name: 123,
-      });
-    } catch (error) {
-      expect(error.message).to.equal('Name must be an array');
-    }
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+        },
+        isAdmin: true
+      }),
+      status: 200
+    });
+    const result = await userMutationResolvers.createCategories(null, {
+      name: 123,
+    },
+    { req: {
+      headers: {
+        authorization: "fakeString"
+      } 
+      }
+    });
+    expect(result).to.deep.equal({ message: 'Name must be an array', statusCode: 400, ok: false });
   });
 
   it('should return an error if the name is empty', async function() {
-    try {
-      await userMutationResolvers.createCategories(null, {
-        name: [],
-      });
-    } catch (error) {
-      expect(error.message).to.equal('Name cannot be empty');
-    }
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+        },
+        isAdmin: true
+      }),
+      status: 200
+    });
+    const result  =await userMutationResolvers.createCategories(null, {
+      name: [],
+    },
+    { req: {
+      headers: {
+        authorization: "fakeString"
+      } 
+      }
+    });
+    expect(result).to.deep.equal({ message: 'Name cannot be empty', statusCode: 400, ok: false });
   });
 
   it('should return an error if the name is null', async function() {
-    try {
-      await userMutationResolvers.createCategories(null, {
-        name: null,
-      });
-    } catch (error) {
-      expect(error.message).to.equal('Name must be an array');
-    }
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+        },
+        isAdmin: true
+      }),
+      status: 200
+    });
+    const result = await userMutationResolvers.createCategories(null, {
+      name: null,
+    },
+
+    { req: {
+      headers: {
+        authorization: "fakeString"
+      } 
+      }
+    });
+    expect(result).to.deep.equal({ message: 'Name must be an array', statusCode: 400, ok: false });
   });
 
   it('should return an if an exception is raised', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+        },
+        isAdmin: true
+      }),
+      status: 200
+    });
     const categoryStub = stub(Category, 'findOne').throws(new Error('Category already exists'));
-    try {
-      await userMutationResolvers.createCategories(null, {
-        name: [
-          'Consumable Products|Food|Vegetables',
-          'Consumable Products|Food|Fruits',
-        ],
-      });
-    } catch (error) {
-      expect(error.message).to.equal('Category already exists');
-      expect(categoryStub.calledOnce).to.be.true;
-    }    
+    const result = await userMutationResolvers.createCategories(null, {
+      name: [
+        'Consumable Products|Food|Vegetables',
+        'Consumable Products|Food|Fruits',
+      ],
+    },
+    { req: {
+      headers: {
+        authorization: "fakeString"
+      } 
+      }
+    });
+    expect(result).to.deep.equal({ message: 'An error occurred!', statusCode: 500, ok: false});
+    expect(categoryStub.calledOnce).to.be.true;
   });
-
 });
