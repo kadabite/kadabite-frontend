c4e17366dbed885844ebedca795bd9f515dbacfe
dominic-source <chinonsodomnic@gmail.com>
2024-06-18 23:24:58 +0100

    update logout test cases

--- express_server/test/test_user_resolver/logout.test.js
+++ express_server/test/test_user_resolver/logout.test.js
@@ -3,59 +3,95 @@ import { stub } from 'sinon';
 import fetch from 'node-fetch';
 import { User } from '../../models/user';
 import { userMutationResolvers } from '../../resolver/user&category.resolver';
+import { Types } from 'mongoose';
 
 describe('logout', () => {
   let findByIdAndUpdateStub;
+  let userId;
+  let fetchStub;
 
-  beforeEach(() => {
-    findByIdAndUpdateStub = stub(User, 'findByIdAndUpdate');
+  before(function() {
+    // stud authRequest
+    fetchStub = stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: userId,
+          id: userId
+          },
+        isAdmin: true
+      }),
+      status: 200
+    });
   });
 
-  afterEach(() => {
+  beforeEach(function() {
+    findByIdAndUpdateStub = stub(User, 'findByIdAndUpdate');
+    userId = new Types.ObjectId().toString();
+  });
+
+  afterEach(function() {
     findByIdAndUpdateStub.restore();
   });
 
+  after(function() {
+    fetchStub.restore();
+  });
+
   it('should return a success message on successful logout', async () => {
-    const userId = '1234567890';
-    const expectedResponse = { message: 'Logged out successfully' };
+    const expectedResponse = {'message': 'Logged out successfully', statusCode: 200, ok: true};
 
     findByIdAndUpdateStub.resolves(true);
 
-    const result = await userMutationResolvers.logout(null, null, { user: { id: userId } });
+    const result = await userMutationResolvers.logout(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+    });
 
     expect(result).to.deep.equal(expectedResponse);
     expect(findByIdAndUpdateStub.calledOnce).to.be.true;
-    expect(findByIdAndUpdateStub.firstCall.args[0]).to.equal(userId);
     expect(findByIdAndUpdateStub.firstCall.args[1]).to.deep.equal({ isLoggedIn: false });
   });
 
   it('should return an error message on failed logout', async () => {
-    const userId = '1234567890';
-    const expectedResponse = { message: 'An error occured' };
-
+    const expectedResponse = {'message': 'Unable to logout user!', statusCode: 400, ok: false };
     findByIdAndUpdateStub.resolves(false);
 
-    const result = await userMutationResolvers.logout(null, null, { user: { id: userId } });
+    const result = await userMutationResolvers.logout(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+    });
 
     expect(result).to.deep.equal(expectedResponse);
     expect(findByIdAndUpdateStub.calledOnce).to.be.true;
-    expect(findByIdAndUpdateStub.firstCall.args[0]).to.equal(userId);
     expect(findByIdAndUpdateStub.firstCall.args[1]).to.deep.equal({ isLoggedIn: false });
   });
 
   it('should throw an error if the findByIdAndUpdate fails', async () => {
-    const userId = '1234567890';
 
     findByIdAndUpdateStub.rejects(new Error('Database error'));
 
-    try {
-      await userMutationResolvers.logout(null, null, { user: { id: userId } });
-      expect.fail('Expected an error to be thrown');
-    } catch (error) {
-      expect(error.message).to.equal('Database error');
-      expect(findByIdAndUpdateStub.calledOnce).to.be.true;
-      expect(findByIdAndUpdateStub.firstCall.args[0]).to.equal(userId);
-      expect(findByIdAndUpdateStub.firstCall.args[1]).to.deep.equal({ isLoggedIn: false });
-    }
+    const result = await userMutationResolvers.logout(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      });
+    expect(result).to.be.deep.equal({ message: 'An error occurred!', statusCode: 500, ok: false });
+    expect(findByIdAndUpdateStub.calledOnce).to.be.true;
+    expect(findByIdAndUpdateStub.firstCall.args[1]).to.deep.equal({ isLoggedIn: false });
   });
 });
