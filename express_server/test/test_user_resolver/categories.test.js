8ad8ddac63a4057fa76cf8e5c2d067ec1e3fa296
dominic-source <chinonsodomnic@gmail.com>
2024-06-19 00:03:05 +0100

    updated testcase for  categories

--- express_server/test/test_user_resolver/categories.test.js
+++ express_server/test/test_user_resolver/categories.test.js
@@ -14,33 +14,91 @@ describe('categories', function() {
   });
 
   it('should return all the categories', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+         }
+      }),
+      status: 200
+    });
+
     const categories = [
-        {
-            id: new Types.ObjectId(),
-            name: 'category1',
-        },
-        {
-            id: new Types.ObjectId(),
-            name: 'category2',
-        },
+      {
+        id: new Types.ObjectId(),
+        name: 'category1',
+      },
+      {
+        id: new Types.ObjectId(),
+        name: 'category2',
+      },
     ]
     const findStub = stub(Category, 'find').resolves(categories);
-    const result = await userQueryResolvers.categories();
-    expect(result).to.deep.equal(categories);
+    const result = await userQueryResolvers.categories(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      });
+    expect(result).to.deep.equal({ categoriesData: categories, statusCode: 200, ok: true });
     expect(findStub.calledOnce).to.be.true;
   });
 
   it('should return an empty array if there are no categories', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+         }
+      }),
+      status: 200
+    });
+
     const findStub = stub(Category, 'find').resolves([]);
-    const result = await userQueryResolvers.categories();
-    expect(result).to.deep.equal([]);
+    const result = await userQueryResolvers.categories(
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      });
+    expect(result).to.deep.equal({ categoriesData: [], statusCode: 200, ok: true });
     expect(findStub.calledOnce).to.be.true;
   });
 
   it('should return an empty array if an exception is raised', async function() {
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: new Types.ObjectId(),
+         }
+      }),
+      status: 200
+    });
     const findStub = stub(Category, 'find').throws(new Error('Error fetching category'));
-    const result = await userQueryResolvers.categories();
-    expect(result).to.deep.equal([]);
+    const result = await userQueryResolvers.categories(
+
+      null,
+      null,
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      }
+    );
+    expect(result).to.deep.equal({ message: 'An error occurred!', statusCode: 500, ok: false });
     expect(findStub.calledOnce).to.be.true;
   });
 });
