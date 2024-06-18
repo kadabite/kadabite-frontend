a6126677d636c221391c2ccd92b077c869239477
dominic-source <chinonsodomnic@gmail.com>
2024-06-18 23:25:40 +0100

    update login test cases

--- express_server/test/test_user_resolver/logins.test.js
+++ express_server/test/test_user_resolver/logins.test.js
@@ -3,18 +3,26 @@ import { stub, restore } from 'sinon';
 import fetch from 'node-fetch';
 import { userMutationResolvers } from '../../resolver/user&category.resolver';
 
-describe('login', () => {
 
-  afterEach(() => {
-    restore();
+describe('login', () => {
+  let fetchStub;
+
+  beforeEach(function() {
+    // Stub fetch.default before each test
+    fetchStub = stub(fetch, 'default');
+  });
+
+  afterEach(function() {
+    // Restore fetch.default stub after each test
+    fetchStub.restore();
   });
 
   it('should return a success message and token on successful login', async () => {
     const email = 'test@example.com';
     const password = 'password123';
-    const expectedResponse = { 'message': 'User logged in successfully', token: 'fakeToken' };
+    const expectedResponse = { message : 'User logged in successfully', token: 'fakeToken', statusCode: 200, ok: true };
 
-    const fetchStub = stub(fetch, 'default').resolves(Promise.resolve({
+    fetchStub.resolves(Promise.resolve({
         ok: true,
         json: () => Promise.resolve({ token: 'fakeToken' })
       }));
@@ -23,28 +31,31 @@ describe('login', () => {
 
     expect(result).to.deep.equal(expectedResponse);
     expect(fetchStub.calledOnce).to.be.true;
-    expect(fetchStub.firstCall.args[0]).to.equal('http://localhost:5000/api/login');
     expect(fetchStub.firstCall.args[1]).to.deep.equal({
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
     });
-
   });
+
 
   it('should return an error message on failed login', async () => {
     const email = 'test@example.com';
     const password = 'wrongpassword';
-    const expectedResponse = { message: 'Login failed' };
-    const fetchStub = stub(fetch, 'default');
+    const expectedResponse = { statusCode: 400, message: "An error occurred!", ok: false };;
 
-    fetchStub.resolves({ ok: false });
+    fetchStub.resolves({
+      ok: false,
+      json: stub().returns({
+        message: "An error occurred!"
+      }),
+      status: 400,
+    });
 
     const result = await userMutationResolvers.login(null, { email, password });
 
     expect(result).to.deep.equal(expectedResponse);
     expect(fetchStub.calledOnce).to.be.true;
-    expect(fetchStub.firstCall.args[0]).to.equal('http://localhost:5000/api/login');
     expect(fetchStub.firstCall.args[1]).to.deep.equal({
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -55,7 +66,6 @@ describe('login', () => {
   it('should throw an error if the fetch request fails', async () => {
     const email = 'test@example.com';
     const password = 'password123';
-    const fetchStub = stub(fetch, 'default');
 
     fetchStub.rejects(new Error('Network error'));
 
@@ -64,7 +74,6 @@ describe('login', () => {
     } catch (error) {
       expect(error.message).to.equal('Network error');
       expect(fetchStub.calledOnce).to.be.true;
-      expect(fetchStub.firstCall.args[0]).to.equal('http://localhost:5000/api/login');
       expect(fetchStub.firstCall.args[1]).to.deep.equal({
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
