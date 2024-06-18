1695ca6464aa78466c8ac2423d9d352637794d45
dominic-source <chinonsodomnic@gmail.com>
2024-06-19 00:03:05 +0100

    updated testcase for  deleteUser

--- express_server/test/test_user_resolver/deleteUser.test.js
+++ express_server/test/test_user_resolver/deleteUser.test.js
@@ -3,12 +3,27 @@ import { stub } from 'sinon';
 import fetch from 'node-fetch';
 import { User } from '../../models/user';
 import { userMutationResolvers } from '../../resolver/user&category.resolver';
+import { Types } from 'mongoose';
 
 describe('deleteUser', () => {
   let findByIdAndUpdateStub;
+  let userId;
 
   beforeEach(() => {
     findByIdAndUpdateStub = stub(User, 'findByIdAndUpdate');
+    userId = new Types.ObjectId().toString();
+    // stud authRequest
+    stub(fetch, 'default').resolves({
+      ok: true,
+      json: stub().returns({
+        user: {
+          _id: userId,
+          id: userId
+          },
+        isAdmin: true
+      }),
+      status: 200
+    });
   });
 
   afterEach(() => {
@@ -16,25 +31,36 @@ describe('deleteUser', () => {
   });
 
   it('should delete a user successfully', async () => {
-    const userId = '123';
-    findByIdAndUpdateStub.resolves();
+    findByIdAndUpdateStub.resolves({id: "fakeid"});
 
-    const result = await userMutationResolvers.deleteUser(null, null, { user: { id: userId } });
+    const result = await userMutationResolvers.deleteUser(
+      userId, { isDeleted: true },
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      });
 
-    expect(result).to.deep.equal({ message: 'User deleted successfully!' });
+    expect(result).to.deep.equal({ message: 'User deleted successfully!', statusCode: 200, ok: true });
     expect(findByIdAndUpdateStub.calledOnce).to.be.true;
     expect(findByIdAndUpdateStub.calledWith(userId, { isDeleted: true })).to.be.true;
   });
 
   it('should handle errors gracefully', async () => {
-    const userId = '123';
     const error = new Error('Something went wrong');
     findByIdAndUpdateStub.rejects(error);
 
-    const result = await userMutationResolvers.deleteUser(null, null, { user: { id: userId } });
+    const result = await userMutationResolvers.deleteUser(
+      userId, { isDeleted: true },
+      { req: {
+        headers: {
+         authorization: "fakeString"
+        } 
+       }
+      });
 
-    expect(result).to.deep.equal({ message: 'An error occurred!' });
+    expect(result).to.deep.equal({'message': 'An error occurred!', statusCode: 500, ok: false });
     expect(findByIdAndUpdateStub.calledOnce).to.be.true;
-    expect(findByIdAndUpdateStub.calledWith(userId, { isDeleted: true })).to.be.true;
   });
 });
