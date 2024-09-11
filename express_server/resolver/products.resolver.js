dc953f2b25acbfa49a2f1c649f598968a60d3ace
dominic-source <chinonsodomnic@gmail.com>
2024-09-11 20:55:59 +0100

    debug product resolver

--- express_server/resolver/products.resolver.js
+++ express_server/resolver/products.resolver.js
@@ -10,7 +10,7 @@ export const productQueryResolver = {
   getProduct: async (_parent, { id }) => {
     try {
       const productData = await Product.findById(id);
-      if(!productData) return { message: 'No product was found!', statusCode: 404, ok: false }
+      if (!productData) return { message: 'No product was found!', statusCode: 404, ok: false }
       return { productData, statusCode: 200, ok: true };
     } catch (error) {
       myLogger.error('Error getting product: ' + error.message);
@@ -40,7 +40,7 @@ export const productQueryResolver = {
   getAllProducts: async (_parent) => {
     try {
       const productsData = await Product.find();
-      if(!productsData) return { message: 'No product was found!', statusCode: 404, ok: false }
+      if (!productsData) return { message: 'No product was found!', statusCode: 404, ok: false }
       return { productsData, statusCode: 200, ok: true };
     } catch (error) {
       myLogger.error('Error getting all products: ' + error.message);
@@ -86,15 +86,19 @@ export const productMutationResolver = {
       name = _.trim(name);
       description = _.trim(description);
       currency = _.trim(currency);
-  
+
       // Get the category information, return error message if false
       const category = await Category.findById(categoryId);
-      if (!category) return {'message': 'The product category ID must be specified!', statusCode: 400, ok: false };
-
-      const productData = new Product({ name, description, price, currency, categoryId: category._id });
+      if (!category) return { 'message': 'The product category ID must be specified!', statusCode: 400, ok: false };
+      // check if product name already exist for the user
+      const userMe = await User.findById(user._id).populate('products');
+      if (userMe.products) {
+        if (userMe.products.find((obj) => obj.name === name)) return { 'message': 'product name already exist', statusCode: 400, ok: false }
+      }
+      const productData = new Product({ name, description, price, currency, categoryId: category._id, userId: user._id });
       await productData.save(); // Save the product first
 
-      const userMe = await User.findById(user._id);
+      // const userMe = await User.findById(user._id);
       userMe.products.push(productData._id);
       await userMe.save();
 
@@ -144,7 +148,7 @@ export const productMutationResolver = {
       // Sanitize users input
       const keys = Object.keys(product);
       const newProduct = keys.filter(key => !(key === 'id' || key === 'createdAt' || key === 'updatedAt')).reduce((obj, key) => {
-        if(key === 'description' || key === 'photo' || key === 'name') {
+        if (key === 'description' || key === 'photo' || key === 'name') {
           obj[key] = _.trim(product[key]);
         } else {
           obj[key] = product[key];
@@ -159,11 +163,11 @@ export const productMutationResolver = {
       if (!category) return { 'message': 'This category does not exist!', statusCode: 404, ok: false };
       // Find the product and update it
       const productData = await Product.findByIdAndUpdate(id, product);
-      if (!productData) return {'message': 'Could not update product!', statusCode: 400, ok: false };
+      if (!productData) return { 'message': 'Could not update product!', statusCode: 400, ok: false };
       return { productData, statusCode: 200, ok: true };
     } catch (error) {
       myLogger.error('Error updating product: ' + error.message);
-      return { message: 'An error occurred!', statusCode: 500, ok: false};
+      return { message: 'An error occurred!', statusCode: 500, ok: false };
     }
   }
 }
