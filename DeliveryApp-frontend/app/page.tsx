da2545b9154e70520717b674d42ce77ba84832d3
dominic-source <chinonsodomnic@gmail.com>
2024-09-16 19:12:17 +0100

    add more components to the landing page

--- DeliveryApp-frontend/app/page.tsx
+++ DeliveryApp-frontend/app/page.tsx
@@ -12,6 +12,14 @@ import { chooseUs, items, restaurant, testimonials } from '@/app/lib/utils';
 import { blue, orange } from '@mui/material/colors';
 import TextField from '@mui/material/TextField';
 import { Typography } from '@mui/material';
+import Imagetext from '@/app/ui/image-text'
+import AddchartIcon from '@mui/icons-material/Addchart';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import AddModeratorIcon from '@mui/icons-material/AddModerator';
+import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload';
+
 
 export default function Page() {
 
@@ -25,18 +33,254 @@ export default function Page() {
     <main className='pt-5 md:pt-0'>
       <section aria-labelledby="hero-heading" className='relative w-full p-12 bg-orange-50 shadow-md bg-[url("/landing_page/meal_mockup3.jpg")] bg-bottom bg-no-repeat bg-[length:100%_50%] md:bg-right md:bg-[length:65%_130%]'>
         <div className="sr-only" id="hero-heading">Healthy Meals Delivered to You</div>
-        <h1 className='text-orange-900 w-full md:text-7xl text-4xl md:tracking-wide tracking-normal md:leading-relaxed leading-normal'>
-          Healthy Eating. <br /> Better Living.
+        <h1 className='pt-5 text-orange-900 md:w-1/2 w-full md:text-6xl text-3xl md:tracking-wide tracking-normal md:leading-tight leading-normal font-semibold'>
+          Experience Seamless Food Delivery with LinuXapp
         </h1>
-        <p className='text-gray-700 w-full md:max-w-sm md:text-xl text-sm md:tracking-wide leading-normal mb-10'>
-          Achieve your health goals effortlessly with fresh, nutritious meals delivered straight to your doorstep.
+        <p className='text-gray-900 w-full md:max-w-sm md:text-lg text-sm md:tracking-wide leading-normal mb-10'>
+            At Linux Kitchens, we revolutionize your food ordering experience by connecting you with local
+            restaurants and vendors effortlessly. With LinuXapp, enjoy quick access to a diverse range of
+            delicious meals delivered right to your doorstep.
         </p>
-        <div className='flex md:flex-row flex-col md:space-x-2 items-center mb-20'>
-          <Button className='bg-orange-500 text-white' aria-label="Order Now for Healthy Meals">Order Now</Button>
-          <div className='mt-auto mb-auto text-gray-900 font-bold' aria-hidden="true"> - or - </div>
-          <Button aria-label="Find Restaurants Near You">Find a Restaurant Near You</Button>
+        <div className='flex flex-row space-x-2 items-center mb-20'>
+          <Button className='bg-orange-500 text-white rounded-lg' aria-label="Order Now for Healthy Meals">Order</Button>
+          <Button aria-label="Learn more about how we operate" className='rounded-lg'>Learn More</Button>
         </div>
       </section>
+
+      <Imagetext ariaLabelledby='feature_section' imageUrl='/landing_page/meal_mockup3.jpg' className='bg-slate-100'>
+        <div className='md:w-1/2 w-full flex flex-col justify-center space-y-2' id='feature_section'>
+          <h2 className='md:text-5xl text-2xl text-gray-700 font-semibold'>Empowering Restaurants: Reach More Customers
+            with Our Comprehensive Delivery solution
+          </h2>
+          <p className='text-gray-800 md:text-lg text-sm'>
+            Our end-to-end solution allows restaurants and food sellers to effortlessly connect with
+            a wider audience. Experience increased visibility and sales while providing your customers
+            with a seamless ordering experience.
+          </p>
+        </div>
+      </Imagetext>
+
+      <section className='relative md:p-28 p-7 space-y-7 flex flex-col justify-center items-center bg-gradient-to-l from-blue-300 to-orange-200 w-full'>
+        <h1 className='md:w-1/2 w-full text-center md:text-5xl text-2xl text-orange-900 font-semibold'>Savor the Flavor, Delivered to Your Door</h1>
+        <p className='md:w-1/2 w-full text-center text-slate-700 text-md'>At Linux Kitchens, we connect you with your favorite local restaurants and food vendors,
+          making ordering food a breeze. Experience the convenience of our LinuXapp and enjoy delicious
+          meals delivered right to you.
+        </p>
+        <div className='flex flex-row space-x-5'>
+          <Button className='bg-orange-500 text-white rounded-lg' aria-label="Download the app">Download</Button>
+          <Button aria-label="Learn more about how we operate" className='rounded-lg'>Learn More</Button>
+        </div>
+      </section>
+
+      <Imagetext ariaLabelledby='feature_section_app' imageUrl='/landing_page/meal_mockup3.jpg' className='bg-orange-50'>
+        <div className='md:w-1/2 w-full flex flex-col justify-center space-y-4' id='feature_section_app'>
+          <AddchartIcon 
+            sx={{ fontSize: 50, color: orange[900] }} 
+            aria-hidden="true"
+          />
+          <h2 className='md:text-5xl text-2xl text-gray-700 font-semibold'>
+            Experience Effortless Food Ordering with LinuXapp
+          </h2>
+          <p className='text-slate-900 text-md'>
+            LinuXapp revolutionizes the way you order food, offering a smooth and intuitive interface
+            that makes your cravings just a few taps away.
+            Enjoy a diverse selection of local vendors and restaurants, all delivered right
+             to your doorstep with unmatched convenience.
+          </p>
+          <div className='flex flex-row space-x-5'>
+            <Button className='bg-orange-500 text-white rounded-lg' aria-label="Order Now for Healthy Meals">Order</Button>
+            <Button aria-label="Learn more about how we operate" className='rounded-lg'>Learn More</Button>
+          </div>
+        </div>
+      </Imagetext>
+
+      <Imagetext ariaLabelledby='feature_section_access' imageUrl='/landing_page/meal_mockup3.jpg' className=''>
+        <div className='md:w-1/2 w-full flex flex-col justify-center space-y-7' id='feature_section_access'>
+          <h2 className='md:text-5xl text-2xl text-gray-700 font-semibold'>
+            Experience Effortless Food Ordering with LinuXapp's User-Friendly Platform
+          </h2>
+          <p className='text-slate-900 text-md'>
+            With LinuXapp, ordering your favourite meals is just a few taps away.
+            Enjoy a hassle-free process that connects you directly to local restaurants and food
+            vendors.
+          </p>
+
+          <div className='flex md:flex-row flex-col md:space-x-5 md:space-y-0 space-y-4'>
+            <div className='flex flex-col space-y-2 shadow-lg bg-blue-100 p-3 rounded-md h-full'>
+              <AcUnitIcon 
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <h3 className='font-semibold text-lg text-orange-900'> Easy Access</h3>
+              <p className='text-sm text-slate-900'>Browse menus, customize orders, and track deliveries all in one place.</p>
+            </div>
+            <div className='flex flex-col space-y-2 shadow-lg bg-blue-100 p-3 rounded-md h-full'>
+              <AddShoppingCartIcon 
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <h3 className='font-semibold text-lg text-orange-900'>Quick Delivery</h3>
+              <p className='text-sm text-slate-900'>Our efficient dispatch riders ensure your food arrives hot and fresh every time.</p>
+            </div>
+          </div>
+        </div>
+      </Imagetext>
+
+      <Imagetext ariaLabelledby='feature_section_dispatcher' imageUrl='/landing_page/meal_mockup3.jpg' className='bg-orange-50'>
+        <div className='md:w-1/2 w-full flex flex-col justify-center space-y-7' id='feature_section_dispatcher'>
+          <h2 className='md:text-5xl text-2xl text-gray-700 font-semibold'>
+            Empowering Dispatch Riders for Flexible Work and Efficient Deliveries
+          </h2>
+          <p className='text-slate-900 text-md'>
+            Our Platform seamlessly integrates dispatch riders, providing them with
+            flexible job opportunities that fit their schedules. This not only enhances their earnings
+            potential but also ensures timely and efficient delivery for our customers
+          </p>
+
+          <div className='flex flex-col space-y-4'>
+            <div className='flex flex-row space-x-2 w-full items-center'>
+              <AssuredWorkloadIcon
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <p className='text-sm text-slate-900'>Flexible schedules for riders to maximize their earnings.</p>
+            </div>
+            <div className='flex flex-row space-x-2 w-full items-center'>
+              <AddModeratorIcon 
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <p className='text-sm text-slate-900'>Streamlined delivery process for a better customer experience.</p>
+            </div>
+            <div className='flex flex-row space-x-2 w-full items-center'>
+              <ApartmentIcon 
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <p className='text-sm text-slate-900'>Join our team and enjoy work-life balance.</p>
+            </div>
+          </div>
+        </div>
+      </Imagetext>
+
+      <section aria-labelledby='discovery' className='md:p-10 p-5 space-y-10'>
+        <div className='flex md:flex-row flex-col md:space-x-10 md:space-y-0 space-y-5'>
+          <h1 className='md:text-4xl text-2xl text-orange-700 font-semibold md:w-1/2 w-full md:tracking-wide tracking-normal md:leading-tight leading-normal' id='discovery'>
+            Discover the Best Local Restaurants and Enjoy Seamless Food Delivery
+          </h1>
+          <p className='self-center text-slate-500 font-bold text-md md:w-1/2 w-full tracking-wide md:leading-snug leading-normal'>
+            At Linux Kitchens, we make it easy to find your favorite local eateries.
+            Our platform allows you to explore variety of restaurants and cuisines right
+            at your fingertips. Enjoy a hassle-free ordering experience that brings
+            delicious meals straight to your door.
+          </p>
+        </div>
+
+        <div className='flex md:flex-row flex-col md:space-x-5 md:space-y-0 space-y-10'>
+          <div className='flex flex-col space-y-2 justify-between'>
+            <AcUnitIcon 
+              sx={{ fontSize: 25, color: orange[900] }} 
+              aria-hidden="true"
+            />
+            <h3 className='font-semibold text-lg text-slate-700'> Effortless Ordering Experience with Just a Few Clicks</h3>
+            <p className='text-sm text-slate-900'>
+              Place your order in seconds and track it in real time.
+            </p>
+            <Link href='/' className='text-slate-700 hover:text-orange-900 focus:text-orange-900 active:text-orange-900 text-sm tracking-tighter font-semibold'>Order &nbsp; &gt;</Link>
+          </div>
+          <div className='flex flex-col space-y-2 justify-between'>
+            <AddShoppingCartIcon 
+              sx={{ fontSize: 25, color: orange[900] }} 
+              aria-hidden="true"
+            />
+            <h3 className='font-semibold text-lg text-slate-700'>
+              Stay Updated with Real-Time Tracking of Your Deliveries
+            </h3>
+            <p className='text-sm text-slate-900'>
+              Know exactly when your food will arrive with our live tracking feature.
+            </p>
+            <Link href='/' className='text-slate-700 hover:text-orange-900 focus:text-orange-900 active:text-orange-900 text-sm tracking-tighter font-semibold'>
+              Track &nbsp; &gt;
+            </Link>
+
+          </div>
+          <div className='flex flex-col space-y-2 justify-between'>
+            <AddShoppingCartIcon 
+              sx={{ fontSize: 25, color: orange[900] }} 
+              aria-hidden="true"
+            />
+            <h3 className='font-semibold text-lg text-slate-700'>
+              Join Our community of Food Lovers and Delivery Experts
+            </h3>
+            <p className='text-sm text-slate-900'>
+              Become part of a vibrant network that supports local food vendors.
+            </p>
+            <Link href='/' className='text-slate-700 hover:text-orange-900 focus:text-orange-900 active:text-orange-900 text-sm tracking-tighter font-semibold'>
+              Join &nbsp; &gt;
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Imagetext ariaLabelledby='hero_section_app' imageUrl='/landing_page/meal_mockup3.jpg' className='bg-orange-50'>
+        <div className='md:w-1/2 w-full flex flex-col justify-center space-y-4' id='hero_section_app'>
+          <h2 className='md:text-5xl text-2xl text-gray-700 font-semibold'>
+            Savor the Best Local Flavors Delivered Fast
+          </h2>
+          <p className='text-slate-900 text-md'>
+            Discover a world of culinary delights at your fingertips with LinuXapp. 
+            We connect you with local restaurants and food vendors for a seamless ordering experience.
+          </p>
+          <div className='flex flex-row space-x-5'>
+            <Button className='bg-orange-500 text-white rounded-lg' aria-label="Order Now for Healthy Meals">Order</Button>
+            <Button aria-label="Learn more about how we operate" className='rounded-lg'>Learn More</Button>
+          </div>
+        </div>
+      </Imagetext>
+
+      <Imagetext ariaLabelledby='feature_section_dispatcher' imageUrl='/landing_page/meal_mockup3.jpg' className='bg-orange-100'>
+        <div className='md:w-1/2 w-full flex flex-col justify-center space-y-7' id='feature_section_dispatcher'>
+          <p className='text-sm text-orange-900'>Benefits</p>
+          <h2 className='md:text-5xl text-2xl text-gray-700 font-semibold'>
+            Unlocking Opportunities for Everyone Involved
+          </h2>
+          <p className='text-slate-900 text-md'>
+            Our Platform empowers customers with a diverse selection of food options,
+            ensuring convenience and satisfaction. Vendors gain increased visibility and sales,
+            while dispatch riders enjoy flexible work opportunities.
+          </p>
+
+          <div className='flex flex-col space-y-4'>
+            <div className='flex flex-row space-x-2 w-full items-center'>
+              <AssuredWorkloadIcon
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <p className='text-sm text-slate-700'>Convenient food delivery at your fingertips.</p>
+            </div>
+            <div className='flex flex-row space-x-2 w-full items-center'>
+              <AddModeratorIcon 
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <p className='text-sm text-slate-700'>Expand your reach and grow your business.</p>
+            </div>
+            <div className='flex flex-row space-x-2 w-full items-center'>
+              <ApartmentIcon 
+                sx={{ fontSize: 25, color: orange[900] }} 
+                aria-hidden="true"
+              />
+              <p className='text-sm text-slate-700'>Flexible jobs that fit your lifestyle.</p>
+            </div>
+          </div>
+          <div className='flex flex-row space-x-5'>
+            <Button aria-label="Learn more about how we operate" className='rounded-lg'>Learn More</Button>
+            <Link href='/' className='self-center text-slate-900 hover:text-orange-700 focus:text-orange-900 active:text-orange-900 text-sm tracking-tighter font-semibold'>
+              Sign Up &nbsp; &gt;
+            </Link>
+          </div>
+        </div>
+      </Imagetext>
 
       <section aria-labelledby="how-it-works-heading" id="how_it_works" className="pt-10">
         <h2 id="how-it-works-heading" className="md:text-7xl text-3xl font-medium text-slate-900 text-center">
@@ -101,7 +345,7 @@ export default function Page() {
         </div>
       </section>
 
-      <section aria-labelledby="why-choose-us-heading" className="w-full bg-slate-100 shadow-2xl" id="why_choose_us">
+      <section aria-labelledby="why-choose-us-heading" className="w-full bg-slate-200 shadow-2xl" id="why_choose_us">
         <h2 id="why-choose-us-heading" className="md:text-7xl text-3xl font-medium text-orange-900 pt-10 text-center">
           <Slidein className=''>Why Choose Us</Slidein>
         </h2>
