2a8c3b7f5a28c3e58dc42d1688273c2d45cf4705
dominic-source <chinonsodomnic@gmail.com>
2024-09-25 18:55:35 +0100

    maintain folders

--- DeliveryApp-frontend/app/ApolloClientProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ApolloProvider } from '@apollo/client';
-import client from '@/lib/apolloClient';
-import { ReactNode } from 'react';
-
-export default function ApolloClientProvider({ children }: { children: ReactNode }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-}
--- express_server/middlewares/logMiddleware.js
+++
@@ -1,6 +0,0 @@
-import { myLogger } from "../utils/mylogger";
-
-export const logMiddleware = (req, res, next) => {
-    myLogger.info(`${new Date().toISOString()} METHOD=${req.method} URL=${req.url} IP=${req.ip}`);
-    next();
-};
