8e5fd896d13bf0f3fa2832cbd2f2c45275566f1f
dominic-source <chinonsodomnic@gmail.com>
2024-10-14 22:05:59 +0100

    update location schema for the user addresse implimentation in signup

--- Next_fullStack_food_Delivery_app/models/location.ts
+++ Next_fullStack_food_Delivery_app/models/location.ts
@@ -1,4 +1,5 @@
-import mongoose, { Document, Schema, Model } from 'mongoose';
+import { ObjectEncodingOptions } from 'fs';
+import mongoose, { Document, Schema, Model, ObjectId } from 'mongoose';
 
 // Define interfaces for each schema
 interface ILocation extends Document {
@@ -9,16 +10,18 @@ interface ILocation extends Document {
 
 interface ILga extends Document {
   name: string;
+  state: mongoose.Schema.Types.ObjectId;
 }
 
 interface IState extends Document {
   name: string;
-  lga: ILga[];
+  lgas: [];
+  country: mongoose.Schema.Types.ObjectId;
 }
 
 interface ICountry extends Document {
   name: string;
-  state: IState[];
+  states: [];
 }
 
 // Define schemas with type annotations
@@ -30,16 +33,41 @@ const locationSchema: Schema<ILocation> = new Schema({
 
 const lgaSchema: Schema<ILga> = new Schema({
   name: { type: String, required: true },
+  state: { type: mongoose.Schema.Types.ObjectId, ref: 'State'}
 });
 
 const stateSchema: Schema<IState> = new Schema({
   name: { type: String, required: true },
-  lga: { type: [lgaSchema], required: true },
+  lgas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lga' }],
+  country: { type: mongoose.Schema.Types.ObjectId, ref: 'Country'}
 });
 
+stateSchema.pre<IState>('save', async function(next) {
+  const countryId = this.country;
+  let country = await Country.findById(countryId as ObjectId);
+  if (country) {
+    const test = country.states.find((item) => item == this._id)
+    if (!test) country.states.push(this._id as never)
+    await country.save();
+    return next();
+  }
+  next();
+});
+
+lgaSchema.pre<ILga>('save', async function(next) {
+  const stateId = this.state;
+  let state = await State.findById(stateId as ObjectId);
+  if (state) {
+    const test = state.lgas.find((item) => item == this._id)
+    if (!test) state.lgas.push(this._id as never)
+    await state.save();
+    return next();
+  }
+  next();
+});
 const countrySchema: Schema<ICountry> = new Schema({
   name: { type: String, required: true },
-  state: { type: [stateSchema], required: true },
+  states: [{ type: mongoose.Schema.Types.ObjectId, ref: 'State' }]
 });
 
 // Create models with type annotations
