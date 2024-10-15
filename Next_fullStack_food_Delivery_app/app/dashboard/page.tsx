226bd2309d509fc8b1ac8aaf3be3b11e6a04a4bc
dominic-source <chinonsodomnic@gmail.com>
2024-10-15 19:20:27 +0100

    use the dashboard page to create location in the database

--- Next_fullStack_food_Delivery_app/app/dashboard/page.tsx
+++ Next_fullStack_food_Delivery_app/app/dashboard/page.tsx
@@ -5,6 +5,9 @@ import withAuth from '@/app/hoc/withAuth';
 import { GET_USERS } from '@/app/query/user.query';
 import LogoutButton from '@/app/ui/logout-button';
 import Loading from '@/app/ui/loading'
+// import { CREATE_NEW_LOCATION } from '@/app/query/location.query';
+// import { useMutation } from '@apollo/client';
+// import { Button } from '@/app/ui/button';
 
 interface User { 
     firstName: string;
@@ -16,6 +19,14 @@ interface User {
 
 const Dashboard = () => {
   const { loading, error, data } = useQuery(GET_USERS);
+
+  // const [setLocation, { loading:myLoading, error: myError, data: myData }] = useMutation(CREATE_NEW_LOCATION, {
+  //   variables: { location: 'Nigeria' },
+  //   onError: (myError) => {
+  //     console.error('Error fetching location:', error);
+  //   },
+  // });
+
   if (loading) {
     return (<Loading />);
   }
@@ -59,6 +70,10 @@ const Dashboard = () => {
         </tbody>
       </table>
       <LogoutButton />
+      {/* <Button className="w-full bg-gray-500 text-orange-900 border border-gray-200" onClick={() => setLocation()}>Set locations </Button>
+      {myData && myData.ok && <p className="text-green-500 text-center">{myData.message}</p>}
+      {myLoading && <p className="text-red-500 text-center">loading ...</p>} */}
+
     </div>
   );
 };
