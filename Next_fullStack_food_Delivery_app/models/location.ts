aa6e05e4613f58e7ef08b0cc8d36dc2b1f0fef1f
dominic-source <chinonsodomnic@gmail.com>
2024-10-15 19:18:59 +0100

    update location models and make it unique

--- Next_fullStack_food_Delivery_app/models/location.ts
+++ Next_fullStack_food_Delivery_app/models/location.ts
@@ -1,11 +1,12 @@
 import { ObjectEncodingOptions } from 'fs';
-import mongoose, { Document, Schema, Model, ObjectId } from 'mongoose';
+import mongoose, { Document, Schema, Model, ObjectId, CallbackError } from 'mongoose';
 
 // Define interfaces for each schema
 interface ILocation extends Document {
   name: string;
   longitude: string;
   latitude: string;
+  userWithThisLocation: mongoose.Schema.Types.ObjectId[];
 }
 
 interface ILga extends Document {
@@ -15,59 +16,71 @@ interface ILga extends Document {
 
 interface IState extends Document {
   name: string;
-  lgas: [];
+  lgas: mongoose.Schema.Types.ObjectId[];
   country: mongoose.Schema.Types.ObjectId;
 }
 
 interface ICountry extends Document {
   name: string;
-  states: [];
+  states: mongoose.Schema.Types.ObjectId[];
 }
 
 // Define schemas with type annotations
 const locationSchema: Schema<ILocation> = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   longitude: { type: String, required: true },
   latitude: { type: String, required: true },
+  userWithThisLocation: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
-const lgaSchema: Schema<ILga> = new Schema({
-  name: { type: String, required: true },
-  state: { type: mongoose.Schema.Types.ObjectId, ref: 'State'}
+const lgaSchema = new Schema<ILga>({
+  name: { type: String, required: true, unique: true },
+  state: { type: mongoose.Schema.Types.ObjectId, ref: 'State', required: true }
 });
 
-const stateSchema: Schema<IState> = new Schema({
-  name: { type: String, required: true },
-  lgas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lga' }],
-  country: { type: mongoose.Schema.Types.ObjectId, ref: 'Country'}
+const stateSchema = new Schema<IState>({
+  name: { type: String, required: true, unique: true },
+  country: { type: mongoose.Schema.Types.ObjectId, ref: 'Country', required: true },
+  lgas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lga' }]
 });
 
-stateSchema.pre<IState>('save', async function(next) {
-  const countryId = this.country;
-  let country = await Country.findById(countryId as ObjectId);
-  if (country) {
-    const test = country.states.find((item) => item == this._id)
-    if (!test) country.states.push(this._id as never)
-    await country.save();
-    return next();
-  }
-  next();
-});
-
-lgaSchema.pre<ILga>('save', async function(next) {
-  const stateId = this.state;
-  let state = await State.findById(stateId as ObjectId);
-  if (state) {
-    const test = state.lgas.find((item) => item == this._id)
-    if (!test) state.lgas.push(this._id as never)
-    await state.save();
-    return next();
-  }
-  next();
-});
 const countrySchema: Schema<ICountry> = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   states: [{ type: mongoose.Schema.Types.ObjectId, ref: 'State' }]
+});
+
+stateSchema.pre<IState>('save', async function (next: (err?: CallbackError | undefined) => void) {
+  try {
+    const countryId = this.country;
+    let country = await Country.findById(countryId as ObjectId);
+    if (country) {
+      const test = country.states.find((item) => item.toString() === (this._id as ObjectId).toString());
+      if (!test) {
+        country.states.push(this._id as ObjectId);
+        await country.save();
+      }
+    }
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  } 
+});
+
+lgaSchema.pre<ILga>('save', async function (next: (err?: CallbackError | undefined) => void) {
+  try {
+    const stateId = this.state;
+    let state = await State.findById(stateId as ObjectId);
+    if (state) {
+      const test = state.lgas.find((item) => item.toString() === (this._id as ObjectId).toString());
+      if (!test) {
+        state.lgas.push(this._id as mongoose.Schema.Types.ObjectId);
+        await state.save();
+      }
+    }
+    next();
+  } catch (error) {
+    next(error as CallbackError);
+  } 
 });
 
 // Create models with type annotations
