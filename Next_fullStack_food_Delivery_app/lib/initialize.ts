ca65ca5186dc889e0beba8de741ee626241c49cf
dominic-source <chinonsodomnic@gmail.com>
2024-10-16 19:26:53 +0100

    integrate connection pooling to mongodb and close idle connection func

--- Next_fullStack_food_Delivery_app/lib/initialize.ts
+++ Next_fullStack_food_Delivery_app/lib/initialize.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import { createClient, RedisClientType } from 'redis';
 
 let isConnected = false;
-let redisClient: RedisClientType;
+let redisClient: RedisClientType | null;
+let idleTimeout: NodeJS.Timeout | null = null;
 
 // Function to initialize the database connection
 export async function initializeDbConnection() {
@@ -19,6 +20,7 @@ export async function initializeDbConnection() {
   try {
     await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 10000, // Increase timeout to 10 seconds
+      maxPoolSize: 50,
     });
     isConnected = true; // Set the connection status to true
     console.log('MongoDB successfully connected');
@@ -43,13 +45,16 @@ export async function initializeRedisClient() {
 
 // Function to initialize all necessary components
 export async function initialize() {
-  await initializeDbConnection();
   await initializeRedisClient();
+  await initializeDbConnection();
 }
 
 // Function to gracefully shut down the application
 async function shutdown() {
   console.log('Shutting down gracefully...');
+  if (idleTimeout) {
+    clearTimeout(idleTimeout);
+  }
   await mongoose.disconnect();
   if (redisClient) {
     await redisClient.quit();
@@ -58,8 +63,25 @@ async function shutdown() {
   process.exit(0);
 }
 
+// Function to handle Idle connections
+function handleIdleConnections() {
+  if (idleTimeout) {
+    clearTimeout(idleTimeout);
+  }
+  idleTimeout = setTimeout(async () => {
+    console.log('Idle timeout reached. Closing connections...');
+    await mongoose.disconnect();
+    if (redisClient) {
+      await redisClient.quit();
+    }
+    isConnected = false;
+    redisClient = null;
+    console.log('Idle connections closed');
+  }, 120000);
+}
 // Handle process signals for graceful shutdown
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
 
+process.on('beforeExit', handleIdleConnections);
 export { redisClient };
